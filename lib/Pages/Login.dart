6e32ea4b79c00b9689b22757e579bcbe3c4caf42
Joao Augusto Rezende <joao.augusto@abas.online>
2023-05-19 23:56:37 -0300

    feat: Completed login page

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:on_buy_auction/Pages/Home.dart';
+import 'package:on_buy_auction/Pages/Register.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool showPass = true;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 45),
-      color: Colors.white,
-      child: Column(
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 115, horizontal: 45),
         children: [
           Row(
             children: [
@@ -58,6 +65,124 @@ class LoginPage extends StatelessWidget {
                   decoration: TextDecoration.none,
                   fontFamily: "Poppins",
                   fontWeight: FontWeight.normal),
+            ),
+          ),
+          const SizedBox(
+            height: 95,
+          ),
+          const TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40))),
+                label: Text(
+                  'Digite seu E-mail',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.5),
+                ),
+                hintText: 'E-mail'),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          TextField(
+            obscureText: showPass,
+            decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      showPass = !showPass;
+                    });
+                  },
+                  icon:
+                      Icon(showPass ? Icons.visibility_off : Icons.visibility),
+                ),
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40))),
+                label: const Text(
+                  'Digite sua senha',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.5),
+                ),
+                hintText: 'Senha'),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const HomePage()));
+            },
+            style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(const Size(0, 50)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                )),
+                backgroundColor: MaterialStateProperty.all(Colors.purple)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Continuar',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Icon(Icons.arrow_right)
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: const [
+              Expanded(
+                child: Divider(
+                  color: Colors.grey,
+                  thickness: 2,
+                  endIndent: 10,
+                ),
+              ),
+              Text('or'),
+              Expanded(
+                child: Divider(
+                  color: Colors.grey,
+                  thickness: 2,
+                  indent: 10,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => const RegisterPage()));
+            },
+            style: ButtonStyle(
+              fixedSize: MaterialStateProperty.all(const Size(0, 50)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Registrar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Icon(Icons.person_2_outlined, color: Colors.purple,)
+              ],
             ),
           ),
         ],
