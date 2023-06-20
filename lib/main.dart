b24f05aa454b9df48b1f256af4847efcb64565b5
Arthur Oliveira Braga <arthuroliveirabraga.12@gmail.com>
2023-06-19 23:03:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ void initState(){
     },
   );
 }
+
+gyroscopeEvents.listen(
+  (GyroscopeEvent event) {
+    print(event);
+  },
+  onError: (error) {
+    // Logic to handle error
+    // Needed for Android in case sensor is not available
+    },
+  cancelOnError: true,
+);
+// [GyroscopeEvent (x: 0.0, y: 0.0, z: 0.0)]
+
 class OpeningPage extends StatelessWidget {
   const OpeningPage({super.key});
 
